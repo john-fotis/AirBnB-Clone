7e15d28e8117254ab237b2b13c086f839ab83d17
petbak98 <petbak98@gmail.com>
2020-08-09 20:09:44 +0300

    Added properties to Listing

--- back-end/AirBnB/src/main/java/com/uoa/AirBnB/model/listingModel/Listing.java
+++ back-end/AirBnB/src/main/java/com/uoa/AirBnB/model/listingModel/Listing.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -16,4 +17,54 @@ public class Listing {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "listing_id", nullable = false)
     private long id;
+    private String title;
+
+    // -- Space --
+    @Enumerated(EnumType.STRING)
+    private RoomType type;
+    private int numOfBeds;
+    private int numOfWc;
+    private int numOfRooms;
+    private boolean livingRoom;
+    private double squareFootage;
+
+    private String description;
+
+    // -- Rules --
+    private boolean smoking;
+    private boolean animals;
+    private boolean parties;
+    private int minRentDays;
+    private int maxGuests;
+
+    // -- Location --
+    private double latitude;
+    private double longitude;
+    private String address;
+    private String neighborhood;
+    private String transportation;
+
+    // -- Cost --
+    private double minCost;
+    private double costPerExtraGuest;
+
+    // -- Facilities --
+    private boolean wifi;
+    private boolean ac;
+    private boolean heating;
+    private boolean kitchen;
+    private boolean tv;
+    private boolean parking;
+    private boolean elevator;
+
+    // -- Availability Days --
+    private Date startDate;
+    private Date endDate;
+
+    // -- Reviews --
+    private int numOfReviews;
+    private double averageRating;
+
+    // reviews, host, photos, messages
+
 }
