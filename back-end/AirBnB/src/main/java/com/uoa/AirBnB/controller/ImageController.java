6ee0f928e1c7245c6bcebe5311d60879f3cca533
petbak98 <petbak98@gmail.com>
2020-09-16 15:22:05 +0300

    Removed Auth from Images

--- back-end/AirBnB/src/main/java/com/uoa/AirBnB/controller/ImageController.java
+++ back-end/AirBnB/src/main/java/com/uoa/AirBnB/controller/ImageController.java
@@ -6,13 +6,12 @@ import com.uoa.AirBnB.service.ImageService;
 import com.uoa.AirBnB.util.Helpers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/images")
-@PreAuthorize("hasRole('GUEST') or hasRole('ADMIN') or hasRole('HOST')")
+//@PreAuthorize("hasRole('GUEST') or hasRole('ADMIN') or hasRole('HOST')")
 public class ImageController {
 
     @Autowired
