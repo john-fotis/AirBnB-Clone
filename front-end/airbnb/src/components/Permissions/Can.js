6ff19e3eed0a226245ba6704cee0506ba1d9ee14
john-fotis <giannisfotis@gmail.com>
2020-09-22 04:10:20 +0300

    deleted uneccessary files

--- front-end/airbnb/src/components/Permissions/Can.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { isAuthorized } from './permissions';
-
-export default function Can({ roles, resource, action, yes, no }) {
-  const hasAccess = isAuthorized(resource, action, roles);
-  return hasAccess ? <>{yes}</> : <>{no}</>;
-}
