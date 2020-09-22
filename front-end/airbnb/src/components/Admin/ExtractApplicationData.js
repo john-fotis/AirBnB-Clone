6cfa0f53c1ba364fa81abb4d971aa2a2b1014de2
john-fotis <giannisfotis@gmail.com>
2020-09-22 03:32:00 +0300

    completed data export for admin, downloadable file created in json and xml

--- front-end/airbnb/src/components/Admin/ExtractApplicationData.js
+++ front-end/airbnb/src/components/Admin/ExtractApplicationData.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import UserService from '../../_services/user.service';
+import 'file-system';
+import 'xml-js';
 
 class ExtractApplicationData extends Component {
   constructor(props){
@@ -10,12 +12,13 @@ class ExtractApplicationData extends Component {
       successful: false
     }
 
-    this.extractJson = this.extractJson.bind(this);
-    this.downloadData = this.downloadData.bind(this);
+    this.extractData = this.extractData.bind(this);
+    this.downloadJson = this.downloadJson.bind(this);
+    this.downloadXML = this.downloadXML.bind(this);
 
   }
 
-  extractJson = () => {
+  extractData = () => {
     UserService.adminExtractEverything()
     .then(response=>{
       this.setState({
@@ -24,31 +27,63 @@ class ExtractApplicationData extends Component {
     })
   }
 
-  downloadData = () => {
-    // const fileDownload = require('js-file-download')
-    // fileDownload(this.state.data, 'ApplicationData.json')
-    console.log(this.state.data.usersList)
+  downloadXML = () => {
+    var convert = require('xml-js');
+    var json = this.state.data;
+    var options = {compact: true, ignoreComment: true, spaces: 4};
+    var xmlContent = convert.json2xml(json, options);
+
+    var pom = document.createElement('a');
+    var filename = "ApplicationData.xml";
+    var bb = new Blob([xmlContent], {type: 'text/plain'});
+    
+    pom.setAttribute('href', window.URL.createObjectURL(bb));
+    pom.setAttribute('download', filename);
+    
+    pom.dataset.downloadurl = ['text/plain', pom.download, pom.href].join(':');
+    pom.draggable = true; 
+    pom.classList.add('dragout');
+    
+    pom.click();
+  }
+
+  downloadJson = () => {
+    const a = document.createElement("a");
+    a.href = URL.createObjectURL(new Blob([JSON.stringify(this.state.data, null, 2)], {
+      type: "text/json;charset=utf-8"
+    }));
+    a.setAttribute("download", "ApplicationData.json");
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
   }
  
   render() { 
     return ( 
       <div className="container">
-        <div className="form-inner">
+        <div className="form-inner" style={{marginTop: '15%'}}>
           <button 
-            onClick={this.extractJson}
+            onClick={this.extractData}
             className="submit-button btn btn-primary btn-block"
             style={{width:'30%', margin: 'auto'}}
-            >Extract JSON
+            >Extract Data
           </button>
           {this.state.data &&(
             <div>
               <h3 style={{textAlign: 'center'}}><br />Extract Succesfull!</h3>
-                <button 
-                onClick={this.downloadData}
+              <button 
+                onClick={this.downloadJson}
                 className="submit-button btn btn-primary btn-block"
                 style={{width:'30%', margin: 'auto'}}
-                >Download
+                >Download Json
               </button>
+              <br />
+              <button 
+                onClick={this.downloadXML}
+                className="submit-button btn btn-primary btn-block"
+                style={{width:'30%', margin: 'auto'}}
+                >Download XML
+              </button>            
             </div>
           )}
         </div>
