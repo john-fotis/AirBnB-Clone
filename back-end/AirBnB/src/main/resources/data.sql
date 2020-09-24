775cfec4815b219ca57fc6c5af6ca3b4f6d85cff
john-fotis <giannisfotis@gmail.com>
2020-09-24 23:57:57 +0300

    added more data to the initial base

--- back-end/AirBnB/src/main/resources/data.sql
+++ back-end/AirBnB/src/main/resources/data.sql
@@ -259,6 +259,133 @@ VALUES(10,2);
 INSERT INTO `airbnb`.`user_roles` (`user_id`,`role_id`)
 VALUES(10,3);
 
+INSERT INTO `airbnb`.`user`
+(
+`user_id`,
+`email`,
+`first_name`,
+`last_name`,
+`password`,
+`username`,
+`number`,
+`user_since`,
+`approved`)
+VALUES
+(11,
+"mpampis@airbnb.test",
+"Mpampis",
+"Deloglou",
+"$2a$10$YHVRGOscVYeMbIjkf5qRg.lYqB43jrIh1baf2SyeI5K3DfL8Mvj4G",
+"mpampis",
+"6990564471",
+NOW(),
+false);
+
+INSERT INTO `airbnb`.`user_roles` (`user_id`,`role_id`)
+VALUES (11,2);
+
+INSERT INTO `airbnb`.`user`
+(
+`user_id`,
+`email`,
+`first_name`,
+`last_name`,
+`password`,
+`username`,
+`number`,
+`user_since`,
+`approved`)
+VALUES
+(12,
+"katerina@airbnb.test",
+"Katerina",
+"Papadopoulou",
+"$2a$10$YHVRGOscVYeMbIjkf5qRg.lYqB43jrIh1baf2SyeI5K3DfL8Mvj4G",
+"katerina",
+"6933564871",
+NOW(),
+true);
+
+INSERT INTO `airbnb`.`user_roles` (`user_id`,`role_id`)
+VALUES (12,2);
+
+INSERT INTO `airbnb`.`user`
+(
+`user_id`,
+`email`,
+`first_name`,
+`last_name`,
+`password`,
+`username`,
+`number`,
+`user_since`,
+`approved`)
+VALUES
+(13,
+"maria@airbnb.test",
+"Maria",
+"Ioannou",
+"$2a$10$YHVRGOscVYeMbIjkf5qRg.lYqB43jrIh1baf2SyeI5K3DfL8Mvj4G",
+"maria",
+"6990564571",
+NOW(),
+null);
+
+INSERT INTO `airbnb`.`user_roles` (`user_id`,`role_id`)
+VALUES (13,3);
+
+INSERT INTO `airbnb`.`user`
+(
+`user_id`,
+`email`,
+`first_name`,
+`last_name`,
+`password`,
+`username`,
+`number`,
+`user_since`,
+`approved`)
+VALUES
+(14,
+"ioanna@airbnb.test",
+"Ioanna",
+"Xaniwth",
+"$2a$10$YHVRGOscVYeMbIjkf5qRg.lYqB43jrIh1baf2SyeI5K3DfL8Mvj4G",
+"ioanna",
+"6997364871",
+NOW(),
+null);
+
+INSERT INTO `airbnb`.`user_roles` (`user_id`,`role_id`)
+VALUES (14,3);
+
+INSERT INTO `airbnb`.`user`
+(
+`user_id`,
+`email`,
+`first_name`,
+`last_name`,
+`password`,
+`username`,
+`number`,
+`user_since`,
+`approved`)
+VALUES
+(15,
+"eugenia@airbnb.test",
+"Eugenia",
+"Krhtikou",
+"$2a$10$YHVRGOscVYeMbIjkf5qRg.lYqB43jrIh1baf2SyeI5K3DfL8Mvj4G",
+"eugenia",
+"6990004871",
+NOW(),
+true);
+
+INSERT INTO `airbnb`.`user_roles` (`user_id`,`role_id`)
+VALUES (15,2);
+INSERT INTO `airbnb`.`user_roles` (`user_id`,`role_id`)
+VALUES (15,3);
+
 /*------------------ Listings --------------------*/
 
 INSERT INTO `airbnb`.`listing`
@@ -802,8 +929,8 @@ VALUES
 1,
 "2020-01-01",
 "2020-12-30",
-0,
-0.0,
+2,
+4.0,
 5
 );
 
@@ -1018,7 +1145,7 @@ VALUES
 1,
 3,
 37.965430,
-23.740248,
+23.740246,
 "Anthippou 54",
 "Greece",
 "Athens",
@@ -1034,11 +1161,401 @@ VALUES
 1,
 1,
 1,
-"2020-01-01",
+"2019-01-01",
 "2020-12-30",
 0,
 0.0,
 10
+);
+
+INSERT INTO `airbnb`.`listing`
+(
+`listing_id`,
+`title`,
+`type`,
+`num_of_beds`,
+`num_of_wc`,
+`num_of_rooms`,
+`living_room`,
+`square_footage`,
+`description`,
+`smoking`,
+`animals`,
+`parties`,
+`min_rent_days`,
+`max_guests`,
+`latitude`,
+`longitude`,
+`address`,
+`country`,
+`city`,
+`postal_code`,
+`neighborhood`,
+`transportation`,
+`min_cost`,
+`cost_per_extra_guest`,
+`wifi`,
+`ac`,
+`heating`,
+`kitchen`,
+`tv`,
+`parking`,
+`elevator`,
+`start_date`,
+`end_date`,
+`num_of_reviews`,
+`average_rating`,
+`user_id`)
+VALUES
+(
+11,
+"NEW luxury flat 80m from metro 3brs",
+"PRIVATE_ROOM",
+2,
+1,
+1,
+0,
+59.5,
+"Newly renovated, easy access to the center of athens",
+0,
+1,
+1,
+1,
+2,
+37.965430,
+23.740228,
+"Georgiou Maridaki",
+"Greece",
+"Athens",
+"11143",
+"Patisia",
+"Train stop closeby",
+60.5,
+33.2,
+1,
+1,
+0,
+1,
+1,
+1,
+0,
+"2019-03-18",
+"2020-11-21",
+2,
+2.0,
+12
+);
+
+INSERT INTO `airbnb`.`listing`
+(
+`listing_id`,
+`title`,
+`type`,
+`num_of_beds`,
+`num_of_wc`,
+`num_of_rooms`,
+`living_room`,
+`square_footage`,
+`description`,
+`smoking`,
+`animals`,
+`parties`,
+`min_rent_days`,
+`max_guests`,
+`latitude`,
+`longitude`,
+`address`,
+`country`,
+`city`,
+`postal_code`,
+`neighborhood`,
+`transportation`,
+`min_cost`,
+`cost_per_extra_guest`,
+`wifi`,
+`ac`,
+`heating`,
+`kitchen`,
+`tv`,
+`parking`,
+`elevator`,
+`start_date`,
+`end_date`,
+`num_of_reviews`,
+`average_rating`,
+`user_id`)
+VALUES
+(
+12,
+"City Center flat at Metro Station",
+"SHARED_ROOM",
+3,
+1,
+1,
+1,
+59.5,
+"Cozy place, with various options for activities during your stay",
+0,
+1,
+1,
+2,
+3,
+37.965430,
+23.740148,
+"Psarwn 21",
+"Greece",
+"Athens",
+"11143",
+"Metaxourgeio",
+"Metro station at 150m",
+45.5,
+30.2,
+1,
+1,
+0,
+1,
+1,
+1,
+0,
+"2020-07-18",
+"2020-12-21",
+3,
+4.0,
+12
+);
+
+INSERT INTO `airbnb`.`listing`
+(
+`listing_id`,
+`title`,
+`type`,
+`num_of_beds`,
+`num_of_wc`,
+`num_of_rooms`,
+`living_room`,
+`square_footage`,
+`description`,
+`smoking`,
+`animals`,
+`parties`,
+`min_rent_days`,
+`max_guests`,
+`latitude`,
+`longitude`,
+`address`,
+`country`,
+`city`,
+`postal_code`,
+`neighborhood`,
+`transportation`,
+`min_cost`,
+`cost_per_extra_guest`,
+`wifi`,
+`ac`,
+`heating`,
+`kitchen`,
+`tv`,
+`parking`,
+`elevator`,
+`start_date`,
+`end_date`,
+`num_of_reviews`,
+`average_rating`,
+`user_id`)
+VALUES
+(
+13,
+"Beautiful central flat with veranda",
+"SHARED_ROOM",
+3,
+1,
+3,
+1,
+54.5,
+"Modern space with great view",
+0,
+1,
+1,
+2,
+3,
+37.965440,
+23.740248,
+"Granikou 47",
+"Greece",
+"Athens",
+"10435",
+"Larissis",
+"Taxi and Tram at 100m",
+65.5,
+50.2,
+1,
+1,
+0,
+1,
+1,
+1,
+0,
+"2020-04-18",
+"2021-02-01",
+2,
+3.0,
+15
+);
+
+INSERT INTO `airbnb`.`listing`
+(
+`listing_id`,
+`title`,
+`type`,
+`num_of_beds`,
+`num_of_wc`,
+`num_of_rooms`,
+`living_room`,
+`square_footage`,
+`description`,
+`smoking`,
+`animals`,
+`parties`,
+`min_rent_days`,
+`max_guests`,
+`latitude`,
+`longitude`,
+`address`,
+`country`,
+`city`,
+`postal_code`,
+`neighborhood`,
+`transportation`,
+`min_cost`,
+`cost_per_extra_guest`,
+`wifi`,
+`ac`,
+`heating`,
+`kitchen`,
+`tv`,
+`parking`,
+`elevator`,
+`start_date`,
+`end_date`,
+`num_of_reviews`,
+`average_rating`,
+`user_id`)
+VALUES
+(
+14,
+"Sunny Penthouse in heart of Athens",
+"FULL_APARTMENT",
+4,
+2,
+3,
+1,
+94.5,
+"Very large space",
+0,
+1,
+1,
+2,
+5,
+37.965433,
+23.740248,
+"Iasonos 17",
+"Greece",
+"Athens",
+"10436",
+"Larissis",
+"Tram, bus and metro options",
+55.5,
+49.2,
+1,
+1,
+0,
+1,
+1,
+1,
+0,
+"2020-04-18",
+"2021-02-01",
+3,
+4.67,
+15
+);
+
+INSERT INTO `airbnb`.`listing`
+(
+`listing_id`,
+`title`,
+`type`,
+`num_of_beds`,
+`num_of_wc`,
+`num_of_rooms`,
+`living_room`,
+`square_footage`,
+`description`,
+`smoking`,
+`animals`,
+`parties`,
+`min_rent_days`,
+`max_guests`,
+`latitude`,
+`longitude`,
+`address`,
+`country`,
+`city`,
+`postal_code`,
+`neighborhood`,
+`transportation`,
+`min_cost`,
+`cost_per_extra_guest`,
+`wifi`,
+`ac`,
+`heating`,
+`kitchen`,
+`tv`,
+`parking`,
+`elevator`,
+`start_date`,
+`end_date`,
+`num_of_reviews`,
+`average_rating`,
+`user_id`)
+VALUES
+(
+15,
+"Studio near Plato's Academy",
+"PRIVATE_ROOM",
+2,
+1,
+2,
+1,
+44.5,
+"Modern place, ideal for young travellers",
+0,
+1,
+0,
+2,
+2,
+37.965430,
+23.740248,
+"Argous 36",
+"Greece",
+"Athens",
+"10441",
+"Larissis",
+"Next to metro station",
+45.5,
+32.2,
+1,
+1,
+0,
+1,
+1,
+1,
+0,
+"2020-04-18",
+"2021-03-11",
+2,
+3.5,
+12
 );
 
 /*------------------ Bookings --------------------*/
@@ -1292,6 +1809,7 @@ VALUES
 "2021-01-14",
 10,
 9);
+
 INSERT INTO `airbnb`.`booking`
 (
 `booking_id`,
@@ -1304,6 +1822,136 @@ VALUES
 "2021-01-15",
 10,
 9);
+
+INSERT INTO `airbnb`.`booking`
+(
+`booking_id`,
+`date`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+21,
+"2020-10-15",
+11,
+13);
+
+INSERT INTO `airbnb`.`booking`
+(
+`booking_id`,
+`date`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+22,
+"2020-10-17",
+12,
+13);
+
+INSERT INTO `airbnb`.`booking`
+(
+`booking_id`,
+`date`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+23,
+"2020-10-20",
+13,
+13);
+
+INSERT INTO `airbnb`.`booking`
+(
+`booking_id`,
+`date`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+24,
+"2020-11-17",
+14,
+13);
+
+INSERT INTO `airbnb`.`booking`
+(
+`booking_id`,
+`date`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+25,
+"2020-12-17",
+15,
+13);
+
+INSERT INTO `airbnb`.`booking`
+(
+`booking_id`,
+`date`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+26,
+"2020-18-17",
+12,
+14);
+
+INSERT INTO `airbnb`.`booking`
+(
+`booking_id`,
+`date`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+27,
+"2020-11-07",
+13,
+14);
+
+INSERT INTO `airbnb`.`booking`
+(
+`booking_id`,
+`date`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+28,
+"2020-12-09",
+14,
+14);
+
+INSERT INTO `airbnb`.`booking`
+(
+`booking_id`,
+`date`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+29,
+"2020-10-17",
+15,
+14);
+
+INSERT INTO `airbnb`.`booking`
+(
+`booking_id`,
+`date`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+30,
+"2020-10-27",
+11,
+14);
 
 /*------------------ Reviews --------------------*/
 
@@ -1385,12 +2033,250 @@ INSERT INTO `airbnb`.`review`
 `user_id`)
 VALUES
 (
-4,
+5,
 "Ηταν πολυ κακό",
 "2020-08-26",
 1,
 5,
 7);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+6,
+"Ηταν πολυ κακό",
+"2020-12-26",
+1,
+11,
+13);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+7,
+"Metria empeiria",
+"2020-12-26",
+3,
+12,
+13);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+8,
+"Arketa kalo",
+"2020-12-26",
+4,
+13,
+13);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+9,
+"Poly kalo, to sytino",
+"2020-12-26",
+5,
+14,
+13);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+10,
+"Tha ksanaphgaina",
+"2020-12-28",
+5,
+14,
+13);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+11,
+"Oxi kai toso katharo",
+"2020-12-26",
+2,
+15,
+13);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+12,
+"Arketa akrivo",
+"2020-12-26",
+3,
+11,
+14);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+13,
+"Poly orea empeiria",
+"2020-12-26",
+5,
+12,
+14);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+14,
+"Arketa kalo",
+"2020-12-30",
+4,
+12,
+14);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+15,
+"Den tha to ksanaekleina",
+"2020-12-26",
+2,
+13,
+14);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+16,
+"Emeina euxaristhmenh",
+"2020-12-26",
+4,
+14,
+14);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+17,
+"Poly eugenikoi idiokthtes kai katharo",
+"2020-12-26",
+5,
+15,
+14);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+18,
+"Kalh empeiria, to sytino",
+"2020-12-26",
+4,
+7,
+13);
+
+INSERT INTO `airbnb`.`review`
+(
+`review_id`,
+`comment`,
+`date`,
+`rating`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+19,
+"Den tha sas apogohteusei",
+"2020-12-26",
+4,
+7,
+14);
 
 /*------------------ Messages --------------------*/
 
@@ -1406,7 +2292,7 @@ INSERT INTO `airbnb`.`message`
 VALUES
 (
 1,
-0,
+1,
 0,
 NOW(),
 "Καλησπέρα σας",
@@ -1429,5 +2315,442 @@ VALUES
 1,
 NOW(),
 "Καλησπέρα και σε εσας",
+1,
+3);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+3,
+0,
+0,
+NOW(),
+"Tha ithela na sas kano mia erotisi",
+1,
+3);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+4,
+0,
+0,
+NOW(),
+"Sxetika me thn aggelia sas",
+1,
+3);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+5,
+0,
+0,
+NOW(),
+"Gia to domatio sth santorini",
+1,
+3);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+6,
+0,
+0,
+NOW(),
+"Kalhspera",
+1,
+13);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+7,
+0,
+0,
+NOW(),
+"Poso konta einai to domatio se stash leoforeiou?",
+1,
+13);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+8,
+0,
+0,
+NOW(),
+"Einai eukolh h prosvash?",
+1,
+13);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+9,
+0,
+0,
+NOW(),
+"Yparxei dynatotha proinou?",
+1,
+13);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+10,
+0,
+0,
+NOW(),
+"Ti ora tha einai etoimaparadoto to domatio th mera pou thelo na to noikiaso?",
+1,
+13);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+11,
+0,
+0,
+NOW(),
+"Geia sas",
+1,
+14);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+12,
+0,
+0,
+NOW(),
+"Yparxoun magazia konta sto diamerisma?",
+1,
+14);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+13,
+0,
+0,
+NOW(),
+"kalhspera sas",
+1,
+8);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+14,
+0,
+0,
+NOW(),
+"Exo mia aporia sxetika me thn aggelia sas",
+1,
+8);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+15,
+0,
+0,
+NOW(),
+"Poso konta einai to domatio sthn paralia?",
+1,
+8);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+16,
+0,
+0,
+NOW(),
+"Epishs, poies einai oi dinatothtes gia faghto konta?",
+1,
+8);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+17,
+0,
+0,
+NOW(),
+"Euxaristw ek twn proterwn",
+1,
+8);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+17,
+0,
+0,
+NOW(),
+"Xairete",
+1,
+9);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+18,
+0,
+0,
+NOW(),
+"Thelo na sas rotisw 2 pragmata gia to dwmatio",
+1,
+9);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+19,
+0,
+0,
+NOW(),
+"Pote tha mporousa na sas kaleso?",
+1,
+9);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+20,
+0,
+0,
+NOW(),
+"Euxaristw",
+1,
+9);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+21,
+0,
+0,
+NOW(),
+"Epishs, poies einai oi dinatothtes gia faghto konta?",
+1,
+14);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+22,
+0,
+0,
+NOW(),
+"peite moy ti ora mporeite na sas kaleso",
+1,
+14);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+23,
+0,
+0,
+NOW(),
+"Euxaristw poly",
+1,
+14);
+
+INSERT INTO `airbnb`.`message`
+(
+`message_id`,
+`seen`,
+`way`,
+`send_date`,
+`text`,
+`listing_id`,
+`user_id`)
+VALUES
+(
+24,
+0,
+0,
+NOW(),
+"Yparxei stash leoforeiou konta?",
 1,
 3);
