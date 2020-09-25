155f3ba80acaa9df877d5ca560b07e2391d104c3
petbak98 <petbak98@gmail.com>
2020-09-25 23:28:29 +0300

    Minor change

--- back-end/AirBnB/src/main/java/com/uoa/AirBnB/util/Recommender/UserListingRatingsTable.java
+++ back-end/AirBnB/src/main/java/com/uoa/AirBnB/util/Recommender/UserListingRatingsTable.java
@@ -19,22 +19,4 @@ public class UserListingRatingsTable {
         listingIndexToId=new ArrayList<Long>();
     }
 
-
-    /*public void appendUserFeatures(Double[][] userFeatures){
-        for(int i=0; i<userIndexToId.size() ; i++){
-            users.get(i).appendRatings(userFeatures[i]);
-        }
-    }
-
-    public void appendListingFeatures(Double[][] listingFeatures){
-        for(int f=0; f<listingFeatures[0].length; f++){
-            UserListingRatings newFeature = new UserListingRatings((long)2147483647);
-
-            for(int a=0; a<listingIndexToId.size(); a++){
-                newFeature.listingRatings[a] = listingFeatures[a][f];
-            }
-
-            users.add(newFeature);
-        }
-    }*/
 }
